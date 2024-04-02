aab59c40f12d91fe842bf61f4361c42d68f363e4
natamartinscodedev <nata.codedev@gmail.com>
2024-04-02 14:13:57 -0300

    commit init navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ export default function Home() {
   return (
     <>
       <header>
-        <nav></nav>
+        <nav className="container_nav-bar container">
+          <h1>Logo</h1>
+          <ul className="conatiner_nav-links">
+            <li><a href="">Home</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">Series</a></li>
+          </ul>
+        </nav>
       </header>
-      <main>
+      {/* <main>
       </main>
       <footer>
-      </footer>
+      </footer> */}
     </>
   );
 }
